1d1fe96de70b1a4b8057de2a3dfb5c6927b005fd
Domenico Cascone <dcascone84@gmail.com>
2020-03-15 20:14:24 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,8 @@
 require "pry"
 def starts_with_a_vowel?(word)
   #binding.pry
-  word.match(/[^\aeiou]/)
+  if word.match(/[^\aeiou]/)
+    true
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
